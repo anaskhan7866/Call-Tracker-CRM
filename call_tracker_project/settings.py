14b09175ce40a120d30f3f9fea3bee6091df3996
Anas Khan <annuskhan7866@gmail.com>
2026-09-21 15:37:16 +0530

    Final Changes

--- call_tracker_project/settings.py
+++ call_tracker_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$o#^$*@=38c*@cmm(r2!n8v2702d*c@uk$izivh7^dqjgh#(&+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Email
